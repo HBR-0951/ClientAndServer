9bbbf1b5d88bf844ef28a4d3c1d52a2dfe779b7d
huangbairu <d0886261@gmail.com>
2022-06-22 16:51:03 +0800

    new

--- ClientAndServer/ClientAndServer/MainClass.cs
+++ ClientAndServer/ClientAndServer/MainClass.cs
@@ -1,3 +1,71 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading;
 
+namespace ClientAndServer
+{
+    public class MainClass
+    {
+        public static void Main()
+        {
+            string ipAddress = "127.0.0.1";
+            int port = 8000;
+
+
+            Thread client = new Thread(() =>
+            {
+                Client p1 = new Client();
+                p1.Connect(ipAddress, port);
+
+                string s = "";
+                string msg = "";
+                do
+                {
+                    msg = p1.GetMsg();
+                    if (msg != "")
+                    {
+                        Console.WriteLine("Client get from Server: " + msg);
+                        msg = "";
+                    }
+
+                    Console.Write("Client write to Server: ");
+                    s = Console.ReadLine();
+                    p1.SendMsg(s);
+                    
+                    Thread.Sleep(100);
+
+                } while (true);
+            });
+            Thread server = new Thread(() =>
+            {
+                Server p2 = new Server();
+                p2.Bind(ipAddress, port);
+
+                p2.Start();
+
+                p2.SendMsg("Hello Client");
+                string msg = "";
+                string s = "";
+                do
+                {
+                    msg = p2.GetMsg();
+                    if(msg != "")
+                    {
+                        Console.WriteLine("Server get from Client: " + msg);
+                        msg = "";
+                    }
+
+                    Console.Write("Server write to Client: ");
+                    s = Console.ReadLine();
+                    p2.SendMsg(s);
+                    Thread.Sleep(100);
+                } while (true);
+
+            });
+            server.Start();
+            client.Start();
+            
+        }
+
+
+    }
+}
--- ClientAndServer/ClientAndServer/Server.cs
+++ ClientAndServer/ClientAndServer/Server.cs
@@ -2,17 +2,21 @@
 using System.Net.Sockets;
 using System.Net;
 
-namespace JungleWars//伺服器端
+namespace ClientAndServer
 {
-    class Program
+    public class Server
     {
-        static void Main(string[] args)
-        {
-            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);//地址型別,流，協議型別
-            //192.168.43.76 本機ip：127.0.0.1
+        public Socket serverSocket;
+        public Socket clientSocket;
 
-            IPAddress ipaddress = IPAddress.Parse("127.0.0.1");
-            IPEndPoint ipendpoint = new IPEndPoint(ipaddress, 8000);
+        public Server(){
+            serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+
+        public void Bind(string ipAddress, int port)
+        {
+            IPAddress ipaddress = IPAddress.Parse(ipAddress);
+            IPEndPoint ipendpoint = new IPEndPoint(ipaddress, port);
 
             try
             {
@@ -24,35 +28,72 @@ namespace JungleWars//伺服器端
                 Console.WriteLine("Bind faild");
                 Console.WriteLine("Warning with " + e.ToString());
             }
-           
+        }
 
+        public void Start()
+        {
             serverSocket.Listen(5);//處理連結佇列個數 為0則為不限制
+            clientSocket = serverSocket.Accept();//接收一個客戶端連結
+        }
+        public string GetMsg()
+        {
+            byte[] dateBuffer = new byte[1024];
 
 
-            Socket clientSocket = serverSocket.Accept();//接收一個客戶端連結
+            int count = clientSocket.Receive(dateBuffer);
 
-            ///向客戶端傳送一條訊息
-            string msg = "Hello client!";
+            string msgReceive = System.Text.Encoding.UTF8.GetString(dateBuffer, 0, count);
+            return msgReceive;
+        }
+
+        public void SendMsg(string msg)
+        {
             byte[] date = System.Text.Encoding.UTF8.GetBytes(msg);//轉換成為bytes陣列
             clientSocket.Send(date);
-
-            string msgReceive;
-
-            do {
-                ///接收一條客戶端的訊息
-                byte[] dateBuffer = new byte[1024];
-
-
-                int count = clientSocket.Receive(dateBuffer);
-
-                msgReceive = System.Text.Encoding.UTF8.GetString(dateBuffer, 0, count);
-                Console.WriteLine(msgReceive);
-            } while (msgReceive != "Q" && msgReceive != "q");
-
-            Console.WriteLine("Connect faild");
-            Console.ReadKey();
-            clientSocket.Close();
-            serverSocket.Close();
         }
+        //static void Main(string[] args)
+        //{
+
+
+        //    IPAddress ipaddress = IPAddress.Parse("127.0.0.1");
+        //    IPEndPoint ipendpoint = new IPEndPoint(ipaddress, 8000);
+
+        //    try
+        //    {
+        //        serverSocket.Bind(ipendpoint);//繫結完成
+        //        Console.WriteLine("Bind successfully");
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        Console.WriteLine("Bind faild");
+        //        Console.WriteLine("Warning with " + e.ToString());
+        //    }
+
+
+
+
+        //    ///向客戶端傳送一條訊息
+        //    string msg = "Hello client!";
+        //    byte[] date = System.Text.Encoding.UTF8.GetBytes(msg);//轉換成為bytes陣列
+        //    clientSocket.Send(date);
+
+        //    string msgReceive;
+
+        //    do {
+        //        ///接收一條客戶端的訊息
+        //        byte[] dateBuffer = new byte[1024];
+
+
+        //        int count = clientSocket.Receive(dateBuffer);
+
+        //        msgReceive = System.Text.Encoding.UTF8.GetString(dateBuffer, 0, count);
+        //        Console.WriteLine(msgReceive);
+        //    } while (msgReceive != "Q" && msgReceive != "q");
+
+        //    Console.WriteLine("Connect faild");
+        //    Console.ReadKey();
+        //    clientSocket.Close();
+        //    serverSocket.Close();
+        //}
     }
 }
