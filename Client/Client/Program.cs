32b0df34aec3524f4bf3d62722ec3a23ffd811ab
huangbairu <d0886261@gmail.com>
2022-06-22 16:02:56 +0800

    new

--- Client/Client/Program.cs
+++ Client/Client/Program.cs
@@ -1,3 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
 
+namespace Tcp客戶端
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ipaddress = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ipendpoint = new IPEndPoint(ipaddress, 8000);
+
+            try
+            {
+                clientSocket.Connect(ipendpoint);
+                Console.WriteLine("Connect successfully");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Connect faild");
+                Console.WriteLine("Waring with " + e.ToString());
+            }
+         
+
+            byte[] date = new byte[1024];
+            int count = clientSocket.Receive(date);
+            string msg = Encoding.UTF8.GetString(date, 0, count);
+            Console.WriteLine(msg);
+
+            string s = Console.ReadLine();
+
+            while (s!= "Q" && s != "q")
+            {
+                clientSocket.Send(Encoding.UTF8.GetBytes(s));
+                s = Console.ReadLine();
+            }
+
+            clientSocket.Close();
+        }
+    }
+}
---
+++ ClientAndServer/ClientAndServer/Client.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace ClientAndServer
+{
+    public class Client
+    {
+        public Socket clientSocket;
+        public Client()
+        {
+            clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+        public void Connect(string ipAddress, int port)
+        {
+            
+            IPAddress ipaddress = IPAddress.Parse(ipAddress);
+            IPEndPoint ipendpoint = new IPEndPoint(ipaddress, port);
+
+            try
+            {
+                clientSocket.Connect(ipendpoint);
+                Console.WriteLine("Connect successfully");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Connect faild");
+                Console.WriteLine("Waring with " + e.ToString());
+            }
+        }
+        public string GetMsg()
+        {
+            byte[] date = new byte[1024];
+            int count = clientSocket.Receive(date);
+            string msg = Encoding.UTF8.GetString(date, 0, count);
+
+            return msg;
+        }
+        public void SendMsg(string msg)
+        {
+            clientSocket.Send(Encoding.UTF8.GetBytes(msg));
+        }
+        public void Close()
+        {
+            clientSocket.Close();
+        }
+
+    }
+}
