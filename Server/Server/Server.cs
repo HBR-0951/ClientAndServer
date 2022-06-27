326a22a1e13204c4da92421762568dca65fc24b2
Waylon <1664940243LWY@gmail.com>
2022-06-27 19:43:44 +0800

    Update:  Received Thread
    更新：資料接收執行緒，從Server移到User物件中進行統一管理

--- Server/Server/Server.cs
+++ Server/Server/Server.cs
@@ -3,113 +3,54 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
-namespace Server
-{
-	public class Server : TcpServer_Template
-	{
-        public int user_Id = 1;
+namespace Server {
+    public class Server : TcpServer_Template {
+        private static UInt64 m_id = 0;
 
-        public string Msg;
+        protected static UInt64 UID_Generator => m_id++;
 
         protected new List<User> m_userList = new(10);
-        
 
-        public Server(string name) :base(name)
-        {
-            
-            //// 創建執行緒，綁定 OnNewConnection方法，並且設定執行緒非在後台運行(此時此刻並未啟動)
-            _awaitClient = new Thread(OnNewConnection) { IsBackground = false };
-            //// 創建執行緒，綁定 OnPacketReceived方法，並且設定執行緒非在後台運行(此時此刻並未啟動)
-            _packetReceived = new Thread(OnPacketReceived) { IsBackground = false };
 
+        public Server(string name) : base(name) {
             OnInitialize();
         }
 
-        protected override void OnInitialize()
-        {
+        protected override void OnInitialize() {
 
         }
 
-        protected override void OnNewConnection()
-        {
+        protected override void OnNewConnection() {
 
             Console.WriteLine("Waiting for Connecting.");
             // 服務器還處於接受連線狀態時，無限迴圈 -> 執行等待客戶
-            while (true)
-            {
-                try
-                {
+            while (true) {
+                try {
                     var userSocket = m_tcpSocket.Accept(); //程式會卡在此處等待客戶端的連線
 
-                    if (userSocket.Connected)
-                    {
+                    if (userSocket.Connected) {
                         Console.WriteLine("Connected");
 
-
-
                         // 創建一個user 給連接到的userSocket
-                        User user = new User(userSocket, user_Id.ToString());
-                        user_Id++;
-                        
-                        SendTo(userSocket, "Hello Client " + (--user_Id) + " !");
+                        var uid = UID_Generator;
+                        var user = new User(userSocket, uid.ToString());
+                        user.Send($"[ Time ] Connect to [ Target : IP ] Successful : [ User_id: {uid} ]");
 
                         //為 user 開啟 一個執行緒，持續等待該用戶的資料
                         _packetReceived.Start(userSocket); // 傳入用戶的 user 作為參數
 
-                        //Console.WriteLine("Send to Client " + (--user_Id) + ": " + Msg.ToUpper());
-                        //SendTo(user, Msg.ToUpper());
 
-                        
                         m_userList.Add(user); //保存用戶
-                        
                     }
-                    
-                }
-                catch(Exception ex)
-                {
+
+                } catch (Exception ex) {
                     Console.WriteLine(ex.Message);
                 }
             }
-            
-        }
-
-        protected override void OnPacketReceived(object? sender)
-        {
-            
-            if (sender == null) return;
-
-
-            var userSocket = (Socket)sender; //強制將目標轉型成 Socket，因為執行緒啟動的時候傳入了Socket型態的參數
-
-            // 服務器 及 客戶端 還處於接受連線狀態時，無限迴圈 -> 等待接受客戶端資料
-            while (userSocket.Connected)
-            {
-                
-                // 連線通道可讀資料 不為 0 時，則讀取資料:否則略過
-                if (userSocket.Available != 0)
-                {
-
-                    // 讀取資料
-                    // Socket.Receive();
-                    byte[] date = new byte[userSocket.Available];
-                    int count = userSocket.Receive(date);
-                    Msg = Encoding.UTF8.GetString(date, 0, count);
-
-
-                    Console.WriteLine("Server get msg: " + Msg);
-
-                    SendTo(userSocket, Msg.ToUpper());
-                }
-                else
-                {
-                    Thread.Sleep(100);
-                }
-
-            }
 
         }
-        public override void OnStart()
-        {
+
+        public override void OnStart() {
             m_tcpSocket.Listen(Backlog); // 開始監聽目標ip位址
 
             _awaitClient.Start(); // 啟動等待客戶端連線執行緒
@@ -131,35 +72,17 @@ namespace Server
         //{
         //    m_tcpSocket.Close();
         //}
-        public void SendTo(User user, string msg)
-        {
-            byte[] bytesPacket;
-            bytesPacket = Encoding.UTF8.GetBytes(msg);
-            Console.WriteLine("Send to User: " + msg);
-            user.SendTo(user, bytesPacket);
-        }
-        public void SendTo(Socket userSocket, string msg)
-        {
-            byte[] bytesPacket;
-            bytesPacket = Encoding.UTF8.GetBytes(msg);
-            Console.WriteLine("Send to Client: " + msg);
-            userSocket.Send(bytesPacket);
-        }
 
         // 廣播
-        public void OnBroadcast(string msg)
-        {
-            byte[] bytesPacket;
-            bytesPacket = Encoding.UTF8.GetBytes(msg);
-            
-            foreach (var user in m_userList)
-            {
-               
-                user.SendTo(user, bytesPacket);
+        public void OnBroadcast(string msg) {
+            Encoding.UTF8.GetBytes(msg);
+
+            foreach (var user in m_userList) {
+                user.Send(msg);
             }
         }
 
-        
+
 
 
 
--- Server/Server/User.cs
+++ Server/Server/User.cs
@@ -1,36 +1,58 @@
-﻿using System;
-using System.Net;
-using System.Net.Sockets;
+﻿using System.Net.Sockets;
 
-namespace Server
-{
-	public class User
-	{
-		private string m_sessionId;
-		private Socket m_tcpSocket;
+namespace Server {
 
-		public User(Socket m_tcpSocket, string session_Id)
-		{
-			this.m_tcpSocket = m_tcpSocket;
-			this.m_sessionId = session_Id;
-		}
 
-		public void SendTo(User target, byte[] bytesPacket)
-        {
-			if (target.m_tcpSocket.Connected)
-			{
-				
-				target.m_tcpSocket.Send(bytesPacket);
-				string s = System.Text.Encoding.UTF8.GetString(bytesPacket, 0, bytesPacket.Length);
-				Console.WriteLine("Send msg: " + s + " to Client:" + target.m_sessionId);
-			}
-		}
+    public class User {
+		private readonly string m_sessionId;
+		private readonly Socket m_tcpSocket;
+		protected Thread PacketReceivedThread { get; private set; }
 
-		public bool IsConnected
-        {
+		public bool IsConnected {
 			get => m_tcpSocket.Connected;
-        }
+		}
 
+		public User(Socket tcpSocket, string session_Id) {
+			this.m_tcpSocket = tcpSocket;
+			this.m_sessionId = session_Id;
+
+			// Thread
+			PacketReceivedThread = new Thread(OnPacketReceived) { IsBackground = false };
+			PacketReceivedThread.Start();
+		}
+
+		// 當封包到達時
+		public virtual void OnPacketReceived() {
+
+			// 服務器 及 客戶端 還處於接受連線狀態時，無限迴圈 -> 等待接受客戶端資料
+			while (m_tcpSocket.Connected) {
+
+				// 連線通道可讀資料 不為 0 時，則讀取資料:否則略過
+				if (m_tcpSocket.Available != 0) {
+
+					// 讀取資料
+					// Socket.Receive();
+					byte[] date = new byte[m_tcpSocket.Available];
+					int count = m_tcpSocket.Receive(date);
+					var msg = System.Text.Encoding.UTF8.GetString(date, 0, count);
+					Console.WriteLine("Server Data Received [Type:Msg]: " + msg);
+
+				} else {
+					Thread.Sleep(100);
+				}
+
+			}
+
+		}
+
+		public void Send(string msg) {
+			if (!m_tcpSocket.Connected) return;
+
+			byte[] bytesPacket;
+			bytesPacket = System.Text.Encoding.UTF8.GetBytes(msg);
+			Console.WriteLine($"Send to [ {m_sessionId} ] : " + msg);
+			m_tcpSocket.Send(bytesPacket);
+		}
 
 	}
 }
