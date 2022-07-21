3f4c2049de15b69c771fdbade34896e13d1eda70
Bo-Ru Huang <d0886261@gmail.com>
2022-07-21 16:20:33 +0800

    rabbitMQ
    依照步驟先寫好 class MQ, class Dispatcher

---
+++ Server/Server/Dispacter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using ProtoBuff.Packet;
+
+namespace Server
+{
+	public delegate void MsgHandler(SamplePacket pack);
+
+	public class Dispacter
+	{
+		public Dispacter()
+		{
+		}
+
+		private Dictionary<string, MsgHandler> m_disp = new();
+
+		public bool Register(string key, MsgHandler value) {
+			// 假設 key 不包含在 dictionary，把他加進去
+            if (! m_disp.ContainsKey(key))
+            {
+				m_disp.Add(key, value);
+				return true;
+            }
+            else
+            {
+				return false;
+            }
+		}
+
+		public bool Dispatch (string key, SamplePacket packet)
+        {
+			// 假設key存在 dictionary 裡
+            if (m_disp.ContainsKey(key))
+            {
+				MsgHandler value = m_disp[key];
+
+				// 執行value（把packet放進 value 裡）
+				if(value != null)
+                {
+					value.Invoke(packet);
+                }
+
+				return true;
+				
+            }
+            else
+            {
+				return false;
+            }
+        }
+	}
+}
+
---
+++ Server/Server/MQ.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using ProtoBuff.Packet;
+namespace Server
+{
+	public class MQ
+	{
+		public MQ(string user_id)
+		{
+			Node temp = new Node();
+			temp.Name = user_id;
+			temp.Type = "123";
+
+			Queues.Add(temp);
+
+			_msgObserver = new Thread(Run);
+			_msgObserver.Start();
+
+		}
+
+		protected Thread? _msgObserver;
+		public List<Node> Queues = new();
+
+		public bool isClose = false;
+
+
+		public struct Node
+		{
+			public string Name { get; set; }
+			public string Type { get; set; }
+			public Queue<SamplePacket> packetQueue { get; set; }
+		}
+
+		
+
+		public void push(SamplePacket msg) { }
+		public void pop() { }
+		public void Run() {
+			// 假設還跟server連接
+            while (!isClose)
+            {
+				// 檢查 List<Node> 中每個 Node
+				foreach (var node in Queues)
+                {
+					// 假設 node 中的queue裡有資料，就pop出來
+					if(node.packetQueue.Count > 0)
+                    {
+						pop();
+						continue;
+                    }
+                }
+				Thread.Sleep(100);
+
+            }
+		}
+
+
+		
+	}
+}
+
