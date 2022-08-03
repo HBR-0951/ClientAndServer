4ddcc9de2b6ce23058a0ffe004dd877d60dfdee6
Bo-Ru Huang <d0886261@gmail.com>
2022-08-03 15:18:51 +0800

    簡易DB連線

--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -8,44 +8,17 @@ namespace Server
     {
         static void Main(string[] args)
         {
-            Server server = new Server("server1"); // 建立一個Server
+            //Server server = new Server("server1"); // 建立一個Server
 
-            server.ListenTo("127.0.0.1", 8000); // 監聽目標位址
-            
-            server.OnStart(); // 啟動服務器(啟動監聽)
+            //server.ListenTo("127.0.0.1", 8000); // 監聽目標位址
 
-            Console.ReadKey();
-            server.OnClose();
+            //server.OnStart(); // 啟動服務器(啟動監聽)
 
-            //bool isQuit = false;
-            //while (!isQuit)
-            //{
-            //    Console.WriteLine("q or Q: quit\n" +
-            //                 "b or B: Broadcast");
-            //    string function = Console.ReadLine();
-            //    switch (function)
-            //        {
-            //            case "Q":
-            //            case "q":
-            //                Console.WriteLine("Quit");
-            //    server.OnClose();
-            //    isQuit = true;
-            //    break;
-            //            case "B":
-            //            case "b":
-            //                Console.WriteLine("Broadcast");
-            //    Console.Write("msg: ");
-            //    string msg = Console.ReadLine();
-            //    server.OnBroadcast(msg);
-            //    break;
-            //    default:
-            //                Console.WriteLine("Write again!");
-            //    break;
+            //Console.ReadKey();
+            //server.OnClose();
 
-            //}
-
-        //}
-
+            TestDB db = new TestDB();
+            db.Run();
 
 
 
---
+++ Server/Server/TestDB.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Data.SqlClient;
+
+namespace Server
+{
+	public class TestDB
+	{
+        protected static MySqlConnection con;
+        protected string sql = "select * from TestDB.Login";
+        protected MySqlCommand mySqlCmd;
+        MySqlConnectionStringBuilder conn_string = new MySqlConnectionStringBuilder();
+
+#pragma warning disable CS8618 // 退出建構函式時，不可為 Null 的欄位必須包含非 Null 值。請考慮宣告為可為 Null。
+        public TestDB()
+#pragma warning restore CS8618 // 退出建構函式時，不可為 Null 的欄位必須包含非 Null 值。請考慮宣告為可為 Null。
+        {
+            conn_string.Server = "127.0.0.1";
+            conn_string.Port = 3306;
+            conn_string.UserID = "root";
+            conn_string.Password = "a0951521314";
+            conn_string.Database = "TestDB";
+
+            con = new MySqlConnection(conn_string.ToString());
+            mySqlCmd = new MySqlCommand(sql, con);
+            
+        }
+
+        public void Run()
+        {
+            try
+            {
+                con.Open();
+
+                var result = mySqlCmd.ExecuteReader();
+
+                while (result.Read())
+                {
+                    Console.WriteLine($"userid: [{result.GetString("userID")}] "
+                                    + $"password: [{result.GetString("password")}] "
+                                    + $"lastLoginTime: [{result.GetString("lastLoginTime")}]");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
+
